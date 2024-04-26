2c0d8692700e67b98ee894bcaebb96dcb6144831
Sullivan Smith <143461406+Sulltothee@users.noreply.github.com>
2024-04-26 11:20:14 +0100

    Add files via upload

---
+++ Battleships/Battleships/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Battleships
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SunkOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SunkOutput
+            // 
+            this.SunkOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.SunkOutput.Location = new System.Drawing.Point(526, 151);
+            this.SunkOutput.Name = "SunkOutput";
+            this.SunkOutput.ReadOnly = true;
+            this.SunkOutput.Size = new System.Drawing.Size(206, 38);
+            this.SunkOutput.TabIndex = 0;
+            this.SunkOutput.Text = "Boats Sunk : 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SunkOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SunkOutput;
+    }
+}
+
---
+++ Battleships/Battleships/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Battleships
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool[,] BackendGrid = new bool[8,8];
+        bool[,] FrontEndGrid = new bool[8, 8];
+        Boat[] allboats;
+        int boatsDestroyed = 0;
+        Random Rand = new Random();
+        public struct Boat
+        {
+            //first int is which coordinate second digit is x / y
+            public int[,] coordinates ;
+            public int length;
+            public int AmountDestroyed;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GenerateBoats();
+            GenerateButtons();
+        }
+        private const int GRID_SIZE = 8;
+
+        private void GenerateButtons()
+        {
+            const int buttonSize = 30;
+            const int spacing = 5;
+
+            for (int row = 0; row < GRID_SIZE; row++)
+            {
+                for (int col = 0; col < GRID_SIZE; col++)
+                {
+                    Button button = new Button();
+                    //makes the button 30 x 30
+                    button.Size = new System.Drawing.Size(buttonSize, buttonSize);
+                    //decides the button's location
+                    button.Location = new System.Drawing.Point(col * (buttonSize + spacing) + 20, row * (buttonSize + spacing) + 20);
+                    button.Name = "(" + row.ToString() + "," + col.ToString() + ")";
+                    //when the button is clicked references the function
+                    button.Click += Button_Click;
+                    Controls.Add(button);
+                }
+            }
+        }
+        void GenerateBoats()
+        {
+            int[] lengths = { 2, 3 , 3,4,5};
+            allboats = new Boat[lengths.Length];
+            for (int i = 0; i < lengths.Length; i++)
+            {
+                Boat freshBoat = new Boat();
+                GenerateBoat(lengths[i], ref freshBoat);
+                allboats[i] = freshBoat;
+            }
+            
+
+        }
+       void GenerateBoat(int length , ref Boat boat )
+        {
+            bool Done = false;
+            while (!Done)
+            {
+                boat = new Boat();
+                boat.length = length;
+                bool RightOrDown = Convert.ToBoolean(Rand.Next(0, 2));
+                boat.coordinates = new int[length, 2];
+                bool valid = true;
+                if (RightOrDown)
+                {
+                    boat.coordinates[0, 0] = Rand.Next(0, 8 - length);
+                    boat.coordinates[0, 1] = Rand.Next(0, 8);
+                    for (int i = 0; i < boat.length; i++)
+                    {
+                        if (BackendGrid[boat.coordinates[0, 0] + i, boat.coordinates[0, 1]] == true) { valid = false; break; }
+                        else { boat.coordinates[0 + i, 0] = boat.coordinates[0, 0] + i; boat.coordinates[0 + i, 1] = boat.coordinates[0, 1]; }
+                    }
+                }
+                else
+                {
+                    boat.coordinates[0, 0] = Rand.Next(0, 8);
+                    boat.coordinates[0, 1] = Rand.Next(0, 8 - length);
+
+                    for (int i = 0; i < boat.length; i++)
+                    {
+                        if (BackendGrid[boat.coordinates[0, 0], boat.coordinates[0, 1] + i] == true) { valid = false; break; }
+                        else { boat.coordinates[0 + i, 0] = boat.coordinates[0, 0]; boat.coordinates[0 + i, 1] = boat.coordinates[0, 1] + i; }
+                    }
+                }
+                if (valid)
+                {
+                    boat.AmountDestroyed = 0;
+                    Done= true;
+                    for (int i = 0; i < boat.length; i++)
+                    {
+                        BackendGrid[boat.coordinates[i, 0], boat.coordinates[i, 1]] = true;
+                        
+                    }
+                }
+            }
+            
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            if (clickedButton != null)
+            {
+                int column = Convert.ToInt32(clickedButton.Name[3]-48);
+                int row = Convert.ToInt32(clickedButton.Name[1]-48);
+                if (BackendGrid[row, column] && !FrontEndGrid[row, column])
+                {
+                    FrontEndGrid[row, column] = true;
+                    clickedButton.BackColor = Color.Red;
+                    //checks each boat
+                    for (int i = 0; i < allboats.Length; i++)
+                    {
+                        //checks all  t he boat coordinates
+                        for (int j = 0; j < allboats[i].length; j++)
+                        {
+                            if (allboats[i].coordinates[j, 0] == row && allboats[i].coordinates[j, 1] == column)
+                            {
+                                allboats[i].AmountDestroyed += 1;
+                                if (allboats[i].AmountDestroyed == allboats[i].length)
+                                {
+                                    boatsDestroyed += 1;
+                                    SunkOutput.Text = ("Boats Sunk : " + boatsDestroyed);
+                                }
+                                i = allboats.Length; break;
+                            }
+                        }
+                    }
+                }
+                else if (!FrontEndGrid[row, column]) { clickedButton.BackColor = Color.Azure; }
+            }
+        }
+
+
+    }
+}
